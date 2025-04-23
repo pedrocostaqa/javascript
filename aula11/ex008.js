735b03bdcf08b1e0302a7c7f53af274130a6fd65
Pedro Costa <198844222+pedrocostaqa@users.noreply.github.com>
2025-04-23 17:11:19 -0300

    exercicios

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 99.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { //Condição simples!
+    console.log('VOCÊ FOI MULTADO!')
+    console.log(`Dirija com cuidado!`)
+}
