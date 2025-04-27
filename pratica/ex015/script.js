3c6eb76877854b8a8328b046b9230185e5af196f
Pedro Costa <198844222+pedrocostaqa@users.noreply.github.com>
2025-04-27 20:54:54 -0300

    ex015

--- pratica/ex015/script.js
+++ pratica/ex015/script.js
@@ -9,11 +9,41 @@ function verificar () {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'um homem'
+            if (idade >= 0 && idade <10){
+                //CRIANÇA
+                img.setAttribute('src', 'criança-m.jpg')
+            } else if (idade < 21) {
+                //JOVEM
+                img.setAttribute('src', 'jovem-m.jpg')
+            } else if (idade < 50) {
+                //ADULTO
+                img.setAttribute('src', 'adulto-m.jpg')
+            } else {
+                //IDOSO
+                img.setAttribute('src', 'idoso-m.jpg')
+            }
         } else {
             genero = 'uma mulher'
+            if (idade >= 0 && idade <10){
+                //CRIANÇA
+                img.setAttribute('src', 'criança-f.jpg')
+            } else if (idade < 21) {
+                //JOVEM
+                img.setAttribute('src', 'jovem-f.jpg')
+            } else if (idade < 50) {
+                //ADULTO
+                img.setAttribute('src', 'adulto-f.jpg')
+            } else {
+                //IDOSO
+                img.setAttribute('src', 'idoso-f.jpg')
+            }
         }
+        res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
